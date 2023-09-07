c14b4503877560e5284ca5b32de5973e01ff9db2
Atul Yadav <atul.yadav@verse.in>
2023-09-07 17:40:54 +0530

    added in color

--- app/src/main/java/com/example/justjava/Color.java
+++ app/src/main/java/com/example/justjava/Color.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class Color extends AppCompatActivity {
